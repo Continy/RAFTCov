f5ebc76a25259a96fd3a3e33796d70e8344f89d4
Continy <satanama.ring@gmail.com>
2023-12-08 22:16:46 +0800

    batch >

--- configs/tartanair.py
+++ configs/tartanair.py
@@ -14,10 +14,10 @@ cfg.seed = 1234
 cfg.log = False
 cfg.sum_freq = 100
 #gaussian:
-cfg.dim = 32
+cfg.dim = 64
 cfg.dropout = 0.1
 
-cfg.num_heads = 2
+cfg.num_heads = 4
 cfg.mixtures = 4
 cfg.gru_iters = 12
 
@@ -26,12 +26,12 @@ cfg.mixed_precision = True
 cfg.optimizer = 'adamw'
 cfg.scheduler = 'OneCycleLR'
 cfg.add_noise = True
-cfg.canonical_lr = 12.5e-4
-cfg.adamw_decay = 1e-4
-cfg.num_steps = 2000
+cfg.canonical_lr = 12.5e-5
+cfg.adamw_decay = 1e-5
+cfg.num_steps = 120000
 cfg.epsilon = 1e-8
 cfg.anneal_strategy = 'linear'
-cfg.batch_size = 4
+cfg.batch_size = 8
 cfg.num_workers = 0
 cfg.autosave_freq = 500
 cfg.training_viz = False
