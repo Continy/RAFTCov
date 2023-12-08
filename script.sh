0c79498762238469848382d90c0a5ab4f56d21c2
Continy <satanama.ring@gmail.com>
2023-12-08 18:56:50 +0800

    slurm bash

---
+++ script.sh
@@ -0,0 +1,1 @@
+python -u /zihao/RAFTCov/train.py --log 
---
+++ slurm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SLURM Resource Parameters
+
+#SBATCH -n 12                       # Number of cores
+#SBATCH -N 1                        # Number of nodes always 1
+#SBATCH -t 3-23:00 # D-HH:MM        # Time using the nodes
+#SBATCH -p a100-gpu-shared               # Partition you submit to
+#SBATCH --gres=gpu:1               # GPUs
+#SBATCH --mem=32G                   # Memory you need
+#SBATCH --job-name=gaussian      # Job name
+#SBATCH -o job_%j.out
+#SBATCH -e job_%j.err
+#SBATCH --mail-type=ALL             # Type of notification BEGIN/FAIL/END/ALL
+#SBATCH --mail-user=satanama.ring@gmail.com
+# Executable
+EXE=/bin/bash
+
+singularity exec --nv --bind /data2/datasets/wenshanw/tartan_data:/zihao/datasets:ro,/data2/datasets/yuhengq/zihao/RAFTCov:/zihao/RAFTCov /data2/datasets/yuhengq/zihao/flowformer.sif bash /zihao/RAFTCov/script.sh
