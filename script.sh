03fdfc2594b956ad3af52c5e2630b95e0c7cbebb
Continy <satanama.ring@gmail.com>
2024-03-22 19:32:22 +0800

    fix script bug

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@ pip install wandb
 pip install --upgrade timm
 export WANDB_API_KEY='need to be filled'
 wandb login $WANDB_API_KEY
-python -u /zihao/RAFTCov/train.py --wandb --stereo --config configs/train/flow_hourglass.yaml --exp 
+python -u /zihao/RAFTCov/train.py --wandb --config configs/train/flow_hourglass.yaml --exp 
